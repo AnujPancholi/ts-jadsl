0e29b573f8cff7b2f173efaa9b8e2de96b9fc379
Anuj Pancholi <anuj.pancholi.1@gmail.com>
2021-02-18 18:31:21 +0530

    test: refactor ListNode test to use toBeNull

--- src/test/ListNode.test.ts
+++ src/test/ListNode.test.ts
@@ -1,11 +1,11 @@
 import { ListNode } from "../index";
 
 describe("#ListNode - constructor", () => {
-  it("should construct new node with given data", () => {
+  test("construct new node with given data", () => {
     const data = 5;
     const freshNode = new ListNode<number>(5);
 
     expect(freshNode.value).toBe(data);
-    expect(freshNode.next).toBe(null);
+    expect(freshNode.next).toBeNull();
   });
 });
