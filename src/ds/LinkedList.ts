d6a9e3a38dd043648f07b5a90bf1afd09fd4a84e
Anuj Pancholi <anuj.pancholi.1@gmail.com>
2021-02-23 22:52:04 +0530

    feat: add overloaded constructor implementation in LinkedList

--- src/ds/LinkedList.ts
+++ src/ds/LinkedList.ts
@@ -4,9 +4,24 @@ class LinkedList<T>{
     head: ListNode<T> | null;
     #length: number;
 
-    constructor(){
+    constructor(initArray: T[]);
+    constructor();
+    constructor(initArray?: T[]){
         this.head = null;
         this.#length = 0;
+        let tail: ListNode<T> | null = null;
+        if(initArray){
+            for(const item of initArray){
+                const freshNode = new ListNode<T>(item);
+                if(tail===null){
+                    this.head = freshNode;
+                } else {
+                    tail.next = freshNode;
+                }
+                tail = freshNode;
+                ++this.#length;
+            }
+        }
     }
 
     length(): number{
--- src/test/LinkedList.test.ts
+++ src/test/LinkedList.test.ts
@@ -1,4 +1,4 @@
-import {LinkedList} from '../index';
+import {LinkedList,ListNode} from '../index';
 
 
 describe('#LinkedList - constructor',() => {
@@ -7,5 +7,23 @@ describe('#LinkedList - constructor',() => {
 
         expect(freshList.head).toBeNull();
         expect(freshList.length()).toBe(0);
+    })
+
+    test("make list initialized with array",() => {
+        const listData = [3,4,5,2,6,1,3];
+        const freshList: LinkedList<number> = new LinkedList<number>(listData);
+
+        expect(freshList.head===null).toBe(false);
+        expect(freshList.length()).toEqual(listData.length);
+
+        let w: ListNode<number> | null = freshList.head,i = 0;
+
+        while(w!==null){
+            expect(w.value).toEqual(listData[i]);
+            ++i;
+            w=w.next;
+        }
+
+        expect(w).toBeNull();
     })
 })
