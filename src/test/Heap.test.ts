812e6f3b324908ae93ee036371bc8f60fab98eb6
Anuj Pancholi <anuj.pancholi.1@gmail.com>
2021-03-15 08:15:31 +0530

    test: for insert method in Heap with a minheap of numbers

--- src/test/Heap.test.ts
+++ src/test/Heap.test.ts
@@ -9,14 +9,22 @@ describe("#Heap - constructor", () => {
   });
 });
 
-describe("#Heap - insert", () => {
+describe("#Heap - insert in minheap of numbers", () => {
   test("should insert value", () => {
     const freshHeap = new Heap<number>((a, b) => a - b);
     const heapData = [5, 6, 3, 7, 2, 0, -3];
+    const sortedHeapData = [...heapData].sort((a, b) => a - b);
 
     freshHeap.insert(heapData[0]);
 
     expect(freshHeap.peek()).toBe(heapData[0]);
     expect(freshHeap.size()).toBe(1);
+
+    for (let i = 1; i < heapData.length; ++i) {
+      freshHeap.insert(heapData[i]);
+    }
+
+    expect(freshHeap.size()).toBe(heapData.length);
+    expect(freshHeap.peek()).toBe(sortedHeapData[0]);
   });
 });
