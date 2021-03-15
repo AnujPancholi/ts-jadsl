59b48f247d6cd0907b1878ab8b202ece870b8e8c
Anuj Pancholi <anuj.pancholi.1@gmail.com>
2021-03-15 08:08:41 +0530

    feat: introduce insert method in Heap with test over minimal functionality

--- src/ds/Heap.ts
+++ src/ds/Heap.ts
@@ -14,11 +14,34 @@ class Heap<T> {
     return Math.floor((childIndex - 1) / 2);
   }
 
+  private _heapifyUp(): void {
+    let currIndex = this._array.length - 1;
+
+    while (
+      currIndex > 0 &&
+      this._comparatorFunction(
+        this._array[this._getParentIndex(currIndex)],
+        this._array[currIndex]
+      ) > 0
+    ) {
+      const temp = this._array[this._getParentIndex(currIndex)];
+      this._array[this._getParentIndex(currIndex)] = this._array[currIndex];
+      this._array[currIndex] = temp;
+      currIndex = this._getParentIndex(currIndex);
+    }
+  }
+
   constructor(
     comparatorFunction: (firstElement: T, secondElement: T) => number
   ) {
     this._array = [];
     this._comparatorFunction = comparatorFunction;
+  }
+
+  insert(value: T): Heap<T> {
+    this._array.push(value);
+    this._heapifyUp();
+    return this;
   }
 
   peek(): T | null {
--- src/test/Heap.test.ts
+++ src/test/Heap.test.ts
@@ -8,3 +8,15 @@ describe("#Heap - constructor", () => {
     expect(freshHeap.size()).toBe(0);
   });
 });
+
+describe("#Heap - insert", () => {
+  test("should insert value", () => {
+    const freshHeap = new Heap<number>((a, b) => a - b);
+    const heapData = [5, 6, 3, 7, 2, 0, -3];
+
+    freshHeap.insert(heapData[0]);
+
+    expect(freshHeap.peek()).toBe(heapData[0]);
+    expect(freshHeap.size()).toBe(1);
+  });
+});
