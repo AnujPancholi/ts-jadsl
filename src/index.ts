b8a60ee44f4e8aa396c2d7cc1880aa0f6596fa05
Anuj Pancholi <anuj.pancholi.1@gmail.com>
2021-05-08 19:06:22 +0530

    feat: add BinarySearchTree to index

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,14 @@ import Stack from "./ds/Stack";
 import Queue from "./ds/Queue";
 import Heap from "./ds/Heap";
 import BinaryTreeNode from "./ds/BinaryTreeNode";
+import BinarySearchTree from "./ds/BinarySearchTree";
 
-export { ListNode, LinkedList, Stack, Queue, Heap, BinaryTreeNode };
+export {
+  ListNode,
+  LinkedList,
+  Stack,
+  Queue,
+  Heap,
+  BinaryTreeNode,
+  BinarySearchTree,
+};
