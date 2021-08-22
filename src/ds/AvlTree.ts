7d99d33693cec6822641996f1221f8c69e0c21e6
Anuj Pancholi <anuj.pancholi.1@gmail.com>
2021-08-22 20:18:47 +0530

    feat: add AvlTree

--- src/ds/AvlTree.ts
+++ src/ds/AvlTree.ts
@@ -11,6 +11,136 @@ class AvlTree<T> extends BinaryTree<T> {
   constructor(keyFunction: (value: T) => number, initArray?: T[]) {
     super();
     this._getKey = keyFunction;
+
+    if (initArray) {
+      for (const value of initArray) {
+        this.root = this._insertValue(this.root, value);
+      }
+    }
+  }
+
+  protected _llRotation(treeNode: BinaryTreeNode<T>): BinaryTreeNode<T> {
+    if (treeNode === null || treeNode.left === null) {
+      throw new Error("CANNOT PERFORM LL ROTATION - NOT ENOUGH NODES");
+    }
+
+    const [nextParent, nextParentRightSubtree] = [
+      treeNode.left,
+      treeNode.left.right,
+    ];
+    nextParent.right = treeNode;
+    treeNode.left = nextParentRightSubtree;
+    treeNode.subtreeHeight =
+      1 +
+      Math.max(
+        treeNode.left?.subtreeHeight || 0,
+        treeNode.right?.subtreeHeight || 0
+      );
+    nextParent.subtreeHeight =
+      1 +
+      Math.max(
+        nextParent.left?.subtreeHeight || 0,
+        nextParent.right?.subtreeHeight || 0
+      );
+    return nextParent;
+  }
+
+  protected _rrRotation(treeNode: BinaryTreeNode<T>): BinaryTreeNode<T> {
+    if (treeNode === null || treeNode.right === null) {
+      throw new Error("CANNOT PERFORM RR ROTATION - NOT ENOUGH NODES");
+    }
+
+    const [nextParent, nextParentLeftSubtree] = [
+      treeNode.right,
+      treeNode.right.left,
+    ];
+    nextParent.left = treeNode;
+    treeNode.right = nextParentLeftSubtree;
+    treeNode.subtreeHeight =
+      1 +
+      Math.max(
+        treeNode.left?.subtreeHeight || 0,
+        treeNode.right?.subtreeHeight || 0
+      );
+    nextParent.subtreeHeight =
+      1 +
+      Math.max(
+        nextParent.left?.subtreeHeight || 0,
+        nextParent.right?.subtreeHeight || 0
+      );
+    return nextParent;
+  }
+
+  protected _lrRotation(treeNode: BinaryTreeNode<T>): BinaryTreeNode<T> {
+    if (
+      treeNode === null ||
+      treeNode.left === null ||
+      treeNode.left.right === null
+    ) {
+      throw new Error("CANNOT PERFORM LR ROTATION - NOT ENOUGH NODES");
+    }
+
+    treeNode.left = this._rrRotation(treeNode.left);
+    return this._llRotation(treeNode);
+  }
+
+  protected _rlRotation(treeNode: BinaryTreeNode<T>): BinaryTreeNode<T> {
+    if (
+      treeNode == null ||
+      treeNode.right === null ||
+      treeNode.right.left === null
+    ) {
+      throw new Error("CANNOT PERFORM RL ROTATION - NOT ENOUGH NODES");
+    }
+
+    treeNode.right = this._llRotation(treeNode.right);
+    return this._rrRotation(treeNode);
+  }
+
+  protected _insertValue(
+    treeNode: BinaryTreeNode<T> | null,
+    value: T
+  ): BinaryTreeNode<T> {
+    if (treeNode === null) {
+      return new BinaryTreeNode<T>(value);
+    }
+
+    if (this._getKey(value) > this._getKey(treeNode.value)) {
+      treeNode.right = this._insertValue(treeNode.right, value);
+    } else {
+      treeNode.left = this._insertValue(treeNode.left, value);
+    }
+
+    const rightSubtreeHeight = treeNode.right?.subtreeHeight || 0;
+    const leftSubtreeHeight = treeNode.left?.subtreeHeight || 0;
+
+    treeNode.subtreeHeight =
+      1 + Math.max(leftSubtreeHeight, rightSubtreeHeight);
+    const balanceFactor = leftSubtreeHeight - rightSubtreeHeight;
+
+    if (balanceFactor === 2) {
+      if (
+        (treeNode.left?.left?.subtreeHeight || 0) -
+          (treeNode.left?.right?.subtreeHeight || 0) >
+        -1
+      ) {
+        treeNode = this._llRotation(treeNode);
+      } else {
+        treeNode = this._lrRotation(treeNode);
+      }
+    } else if (balanceFactor === -2) {
+      if (
+        (treeNode.right?.left?.subtreeHeight || 0) -
+          (treeNode.right?.right?.subtreeHeight || 0) <
+        1
+      ) {
+        treeNode = this._rrRotation(treeNode);
+      } else {
+        treeNode = this._rlRotation(treeNode);
+      }
+    }
+
+    return treeNode;
   }
 }
 
--- test/AvlTree.test.ts
+++ test/AvlTree.test.ts
@@ -1,10 +1,61 @@
 import AvlTree from "../src/ds/AvlTree";
+import BinaryTreeNode from "../src/ds/BinaryTreeNode";
 
 describe("#AvlTree - constructor", () => {
+  function assertBalancedBst<T>(
+    treeNode: BinaryTreeNode<T> | null,
+    keyFunction: (value: T) => number
+  ) {
+    expect(treeNode).not.toBeNull();
+    expect(treeNode?.isBalanced()).toBe(true);
+    expect(treeNode?.isBinarySearchTree(keyFunction)).toBe(true);
+  }
+
   test("should construct empty tree if only key function passed", () => {
     const numAvlTree = new AvlTree<number>((n: number) => n);
 
     expect(numAvlTree.root).toBeNull();
     expect(numAvlTree.getPreorderTraversal()).toEqual([]);
+  });
+
+  test("should construct balanced BST when initArray passed", () => {
+    interface InsertionTestCase {
+      input: number[];
+      expectedRootValue: number;
+      expectedPostorder: number[];
+    }
+    const cases: Array<InsertionTestCase> = [
+      {
+        input: [1, 2, 3, 4, 5, 6],
+        expectedRootValue: 4,
+        expectedPostorder: [1, 3, 2, 6, 5, 4],
+      },
+      {
+        input: [5, 4, 3, 2, 1],
+        expectedRootValue: 4,
+        expectedPostorder: [1, 3, 2, 5, 4],
+      },
+      {
+        input: [20, 15, 17],
+        expectedRootValue: 17,
+        expectedPostorder: [15, 20, 17],
+      },
+      {
+        input: [20, 25, 22],
+        expectedRootValue: 22,
+        expectedPostorder: [20, 25, 22],
+      },
+    ];
+
+    for (let i = 0; i < cases.length; ++i) {
+      const testCase: InsertionTestCase = cases[i];
+      const numAvlTree = new AvlTree<number>((n: number) => n, testCase.input);
+
+      expect(numAvlTree.root?.value).toBe(testCase.expectedRootValue);
+      assertBalancedBst<number>(numAvlTree.root, (n: number) => n);
+      expect(numAvlTree.getPostorderTraversal()).toEqual(
+        testCase.expectedPostorder
+      );
+    }
   });
 });
