aea4c1f5b1b242284bad7532b168707ad4274bb0
Anuj Pancholi <anuj.pancholi.1@gmail.com>
2021-08-29 17:10:39 +0530

    feat: add additional ds classes to export

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,9 @@ import Stack from "./ds/Stack";
 import Queue from "./ds/Queue";
 import Heap from "./ds/Heap";
 import BinaryTreeNode from "./ds/BinaryTreeNode";
+import BinaryTree from "./ds/BinaryTree";
 import BinarySearchTree from "./ds/BinarySearchTree";
+import AvlTree from "./ds/BinarySearchTree";
 
 export {
   ListNode,
@@ -13,5 +15,7 @@ export {
   Queue,
   Heap,
   BinaryTreeNode,
+  BinaryTree,
   BinarySearchTree,
+  AvlTree,
 };
